80b17d05c9fc5f34f0124669700bae32812d3830
Sultan Siddiqi <sultan.siddiqi@gmail.com>
2022-03-20 15:22:30 -0400

    testing fix of webpage links

--- assets/js/end.js
+++ assets/js/end.js
@@ -30,7 +30,7 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    window.location.assign('https://sultansiddiqi.github.io/Challenge-004---Web-APIs-Challenge-Code-Quiz/assets/html/highscores.html')
 
     
 }
